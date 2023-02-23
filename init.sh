aaf0cead7e2f16fbf0c6260ae4b9ffc1f07c5df8
0xNiel <123564022+0xNiel@users.noreply.github.com>
2023-02-23 09:18:05 -0500

    cleaned init

--- init.sh
+++ init.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-sudo yum update –y
+sudo yum update -y
 sudo wget -O /etc/yum.repos.d/jenkins.repo \
     https://pkg.jenkins.io/redhat-stable/jenkins.repo
 sudo rpm --import https://pkg.jenkins.io/redhat-stable/jenkins.io.key
