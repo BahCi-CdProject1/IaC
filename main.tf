15f0bdd94a4870353e4a40f651cd2e238041b06f
0xNiel <123564022+0xNiel@users.noreply.github.com>
2023-02-28 09:31:07 -0500

    fix 2

--- main.tf
+++ main.tf
@@ -55,9 +55,13 @@ resource "aws_route_table" "hoh-app-web-rt" {
   vpc_id = "${aws_vpc.hoh-app-vpc.id}"
   route = {
     cidr_block = "0.0.0.0/0"
-    gateway_id = "${aws_internet_gateway.hoh-app-igw.id}"
-  } 
+    gateway_id = aws_internet_gateway.hoh-app-igw.id
+  }
 }
+
+# resource "aws_route" "main-route" {
+  
+# }
 resource "aws_route_table_association" "prod" {
   subnet_id = aws_subnet.SubnetProd
   route_table_id = aws_route_table.hoh-app-web-rt
