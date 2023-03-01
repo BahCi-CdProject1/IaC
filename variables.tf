bedbfd893564a744e1742525313a6a9ff32c37db
0xNiel <123564022+0xNiel@users.noreply.github.com>
2023-03-01 14:14:14 -0500

    Got Terraform working

--- variables.tf
+++ variables.tf
@@ -1,12 +1,17 @@
 variable "region" {
-  type = string
+  type    = string
   default = "us-east-1"
 }
 
 variable "images" {
-  type = map
+  type = map(any)
   default = {
     "PROD" = "ami-06deb6bd572fb29e9"
-    "DEV" = "ami-0d3a94f66b42eaa0f"
+    "DEV"  = "ami-0d3a94f66b42eaa0f"
   }
+}
+
+variable "my_profile" {
+  type    = string
+  default = "Add_Profile_Here"
 }
